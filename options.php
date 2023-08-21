45f80f90efe834bd475f8c26b2c6cff68d3d8021
Pavel Poliakov <89504082116@mail.ru>
2023-08-21 23:43:21 +0700

    Create options.php

---
+++ options.php
@@ -0,0 +1,10 @@
+<?php
+  # Определяем все необходимые константы
+
+	define("UNITPAY_PUBLIC_KEY", "731004-3m729"); # Публичный ключ unitpay
+	define("UNITPAY_SECRET_KEY", "19mf738l3dakd882jdkkdk29277dh"); # Секретный ключ
+	define("UNITPAY_IP", "31.186.100.49"); # IP адрес сервера unitpay в России
+	define("UNITPAY_URL", "https://unitpay.ru/api"); # Url адрес API unitpay
+	define('LOGIN_UNITPAY', 'yourlogin@gmail.com'); # Ваш логин в unitpay
+	define('UNITPAY_PROJECT_ID', 990187); # ID Вашего проекта
+	define("UNITPAY_CURRENCY", "RUB"); # Валюта платежей
